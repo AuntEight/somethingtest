531618be613a45b67ddf3e7f42b2855911e3a393
AuntEight <weibozhixiaoguan@gmail.com>
2014-02-28 23:54:50 -0600

    add the smoke test

---
+++ cmpt371project_Smoke/src/com/example/cmpt371project/test/LocationEditTest.java
@@ -0,0 +1,30 @@
+package com.example.cmpt371project.test;
+
+import com.example.cmpt371project.locationEdit;
+import com.robotium.solo.Solo;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+public class LocationEditTest extends
+		ActivityInstrumentationTestCase2<locationEdit> {
+			
+	private Solo solo;		
+
+	public LocationEditTest() {
+		super(locationEdit.class);
+	}
+
+	protected void setUp() throws Exception {
+		solo = new Solo(getInstrumentation(),getActivity());
+		
+		super.setUp();
+	}
+	
+	/**
+	 * try to save all wired data
+	 */
+	public void testSave(){
+		//
+	}
+
+}
---
+++ cmpt371project_Smoke/src/com/example/cmpt371project/test/locationListTest.java
@@ -0,0 +1,59 @@
+package com.example.cmpt371project.test;
+
+import com.example.cmpt371project.childrenAtLocation;
+import com.example.cmpt371project.childrenList;
+import com.example.cmpt371project.locationEdit;
+import com.example.cmpt371project.locationList;
+import com.robotium.solo.Solo;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+public class locationListTest extends
+		ActivityInstrumentationTestCase2<locationList> {
+			
+	private Solo solo;
+
+	public locationListTest() {
+		super(locationList.class);
+	}
+
+	protected void setUp() throws Exception {
+		solo = new Solo(getInstrumentation(), getActivity());
+		super.setUp();
+	}
+	
+	/**
+	 * test add 
+	 */
+	public void testAdd(){
+		solo.clickOnButton("Add");
+		solo.sleep(1000);
+		solo.assertCurrentActivity("Should jump to Location_edit activity", locationEdit.class);
+	    solo.goBackToActivity("locationList");	    
+	}
+	
+	/**
+	 * test children button
+	 */
+	public void testChildren(){
+		solo.clickOnButton("Children");
+		solo.sleep(1000);
+		solo.assertCurrentActivity("Should jump to children_list activity", childrenList.class);
+	    solo.goBackToActivity("locationList");			
+	}
+	
+	/**
+	 * test tap on a location button
+	 */
+	public void testTapingLocation(){
+		solo.clickInList(1);
+		solo.sleep(1000);
+		solo.assertCurrentActivity("Should jump to childrenAtLocation activity", childrenAtLocation.class);
+	    solo.goBackToActivity("locationList");	
+	}
+	
+	/**
+	 * test search field
+	 */
+
+}
